db4ec60ab5f4f7392e8c09e027d559056e356408
Kyumin Han <4rbals30@naver.com>
2021-12-29 00:16:04 +0900

    List view content

---
+++ LandMarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  LandMarks
+//
+//  Created by Kyumin Han on 2021/12/29.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
