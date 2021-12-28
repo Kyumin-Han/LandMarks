52a90552dd2153c26a1eea0cfbf05ca11fb199b2
Kyumin Han <4rbals30@naver.com>
2021-12-28 20:15:12 +0900

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Kyumin Han on 2021/12/28.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
